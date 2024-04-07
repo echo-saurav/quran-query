fd87f2f2d2a20e4e01ce88fc70f38c5b0e1e5d2a
saurav <echo.saurav@gmail.com>
2024-04-07 22:39:56 +0600

    text env

--- src/app/Home/Hompage.js
+++ src/app/Home/Hompage.js
@@ -2,11 +2,12 @@
 import { Typography } from "antd";
 import Welcome from "./Welcome";
 
-export default function HomePage({text}) {
+export default function HomePage({text,api}) {
   return (
     <main >
       <div>
         <Typography.Title>{text}</Typography.Title>
+        <Typography.Title>{api}</Typography.Title>
         <Welcome/>
       </div>
     </main>
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import HomePage from "./Home/Hompage";
 export default function Home() {
   return (
     <main>
-      <HomePage text={process.env.TEXT}/>
+      <HomePage 
+      api={process.env.WEAVIATE_API}
+      text={process.env.TEXT}/>
     </main>
   );
 }
