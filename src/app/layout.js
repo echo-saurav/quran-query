dfe222b4e1102da3dc7c79db5c5de328641493c6
saurav <echo.saurav@gmail.com>
2024-04-08 10:26:10 +0600

    Mon Apr  8 10:26:10 +06 2024

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import "./globals.css";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import Root from "./root";
+import prev_image from "./prev.jpg"
 
 
 export const metadata = {
   title: "Al Quran",
   description: "Explore Quran in different way",
+  image: prev_image
 };
 
 
